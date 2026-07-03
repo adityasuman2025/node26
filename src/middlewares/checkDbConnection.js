a2d1230a8d2d899d5c3b50c32ce1bc2d76b083cc
adityasuman2025 <adityasuman2025@gmail.com>
2026-07-03 19:41:36 +0530

    vercel deployment

--- src/middlewares/checkDbConnection.js
+++ src/middlewares/checkDbConnection.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
-import { errorCatch } from "../utils/index.js"
+import { errorCatch } from "../utils/index.js";
+import dbConnection from "../db.js";
 
-export default function checkDbConnection(req, res, next) {
+export default async function checkDbConnection(req, res, next) {
     try {
-        if (mongoose.connection.readyState !== 1) throw new Error("database is not connected");
+        if (mongoose.connection.readyState !== 1) {
+            await dbConnection; // wait for connection to establish on cold starts
+        }
         next();
     } catch (err) {
         errorCatch(err, req, res);
